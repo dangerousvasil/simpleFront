64bd848f9801de37fe9d133628b869d91761094a
pvv <79067000905@ya.ru>
2017-10-20 15:32:05 +0300

    simple Front

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-require_once 'vendor/autoload.php';
-
-use JsonRPC\Client;
 
 if ($_GET['info'] ?? false) {
     phpinfo();
@@ -9,7 +6,24 @@ if ($_GET['info'] ?? false) {
     $appConfig = '/etc/application/config.ini';
     $conf = parse_ini_file($appConfig, true);
 
-    var_dump($conf);
+    $serviceHost = $conf['application']['service.host'][0];
+
+    // 1. инициализация
+    $ch = curl_init();
+
+    // 2. устанавливаем опции, включая урл
+    curl_setopt($ch, CURLOPT_URL, 'http://' . $serviceHost);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+
+    $header[] = "Host: service";
+    @curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    // 3. выполнение запроса и получение ответа
+    $output = curl_exec($ch);
+
+    var_dump($output);
+    // 4. очистка ресурсов
+    curl_close($ch);
 
 }
 
