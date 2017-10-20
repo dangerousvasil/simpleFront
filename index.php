96e9a1fa7e746453f4dc9414effe54e4de28582c
pvv <79067000905@ya.ru>
2017-10-20 15:36:04 +0300

    simple Front

--- index.php
+++ index.php
@@ -8,23 +8,30 @@ if ($_GET['info'] ?? false) {
 
     $serviceHost = $conf['application']['service.host'][0];
 
-    // 1. инициализация
-    $ch = curl_init();
+    var_dump($serviceHost);
 
-    // 2. устанавливаем опции, включая урл
-    curl_setopt($ch, CURLOPT_URL, 'http://' . $serviceHost);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 1);
+    try {
 
-    $header[] = "Host: service";
-    @curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-    // 3. выполнение запроса и получение ответа
-    $output = curl_exec($ch);
 
-    var_dump($output);
-    // 4. очистка ресурсов
-    curl_close($ch);
+        // 1. инициализация
+        $ch = curl_init();
 
+        // 2. устанавливаем опции, включая урл
+        curl_setopt($ch, CURLOPT_URL, 'http://' . $serviceHost);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+
+        $header[] = "Host: service";
+        @curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        // 3. выполнение запроса и получение ответа
+        $output = curl_exec($ch);
+
+        var_dump($output);
+        // 4. очистка ресурсов
+        curl_close($ch);
+    } catch (Exception $e) {
+        var_dump($e);
+    }
 }
 
 
