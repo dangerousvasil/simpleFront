97c7ea036f43c5d8f3adac9a668ef5dc5f96393b
pvv <79067000905@ya.ru>
2017-10-20 14:50:46 +0300

    simple Front

--- index.php
+++ index.php
@@ -6,19 +6,11 @@ use JsonRPC\Client;
 if ($_GET['info'] ?? false) {
     phpinfo();
 } else {
-    // here set up balanced nginx
-    $client = new Client('http://web:1080/');
-    $http = $client->getHttpClient()
-                   ->withDebug()
-                   ->withHeaders(['Host: service']);
+    $appConfig = '/etc/application/config.ini';
+    $conf = parse_ini_file($appConfig, true);
 
-    try {
-        $result = $client->execute('addition', [3, 5]);
-    } catch (Exception $e) {
-        // debug info is disgusting
-        var_dump($e);
-    }
-    var_dump($result);
+    var_dump($conf);
+
 }
 
 
